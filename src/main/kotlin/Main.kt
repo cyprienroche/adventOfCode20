5aef3d1a0a5ea6ac76a58173faf16549a4951b1e
Cyprien <roche.cyprien@gmail.com>
2020-12-01 15:06:05 +0000

    expect program arguments in the form [day, i/o] and solves problem for the given day

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,23 @@
 import java.io.File
 import java.io.PrintStream
-import java.util.*
-
+import java.util.Scanner
 
 fun main(args: Array<String>) {
-    var scanner: Scanner
-    var printStream: PrintStream
-
-    if (args.isEmpty() || args[0] == "System") {
-        scanner = Scanner(System.`in`)
-    } else {
-        val fileIn = File(args[0])
-        scanner = Scanner(fileIn)
+    // expecting arguments of form: [day, i/o]
+    val day = if (args.isEmpty()) "" else args[0]
+    val dayStringNumber = Regex("[0-9]+").find(day)
+    if (dayStringNumber == null || dayStringNumber.value.toInt() !in 1..25) {
+        println("Please enter a day number between 1 and 25")
+        return
     }
+    val dayNumber = dayStringNumber.value.toInt()
 
-    if (args.size < 2 || args[1] == "System") {
-        printStream = PrintStream(System.out)
-    } else {
-        val fileOut = File(args[1])
-        printStream = PrintStream(fileOut)
-    }
+    val path = "src/main/resources/day$dayNumber/"
+    val scanner = if (args.size == 1) Scanner(System.`in`) else Scanner(File(path + "input.txt"))
+    val printStream = if (args.size == 1) PrintStream(System.out) else PrintStream(File(path + "output.txt"))
 
-    println("process complete.\nexit...")
+    DayProblemFactory.getDayProblem(dayNumber, scanner, printStream).solve()
+
+    println("process complete.")
     println("exit...")
 }
