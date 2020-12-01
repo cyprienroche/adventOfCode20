48085a7826cb9d2b444721c418614b49801bfd2f
Cyprien <roche.cyprien@gmail.com>
2020-12-01 17:48:23 +0000

    add tests for day1

---
+++ src/test/kotlin/day1/Day1Test.kt
@@ -0,0 +1,55 @@
+package day1
+
+import getScanner
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.test.assertEquals
+
+class Day1Test {
+
+    private val outContent = ByteArrayOutputStream()
+    private val originalOut = System.out
+
+    @Before
+    fun setupStreams() {
+        System.setOut(PrintStream(outContent))
+    }
+
+    @After
+    fun restoreStreams() {
+        System.setOut(originalOut)
+    }
+
+    @Test
+    fun `can find correct pair that sums to target`() {
+        val pair = findPairThatSumsToTarget(listOf(1, 2, 3, 4, 5), 9)
+        assertEquals(Pair(4, 5), pair)
+    }
+
+    @Test
+    fun `can find correct triple that sums to target`() {
+        val triple = findTripleThatSumsToTarget(listOf(1, 2, 3, 4, 5), 12)
+        assertEquals(Triple(3, 4, 5), triple)
+    }
+
+    @Test
+    fun `can load example input and print correct output for pair`() {
+        val scanner = getScanner(1, "test")
+        val printStream = PrintStream(outContent)
+        Day1(scanner, printStream).solvePart1()
+        val output = outContent.toString().trimMargin().toInt()
+        assertEquals(514579, output)
+    }
+
+    @Test
+    fun `can load example input and print correct output for triple`() {
+        val scanner = getScanner(1, "test")
+        val printStream = PrintStream(outContent)
+        Day1(scanner, printStream).solvePart2()
+        val output = outContent.toString().trimMargin().toInt()
+        assertEquals(241861950, output)
+    }
+}
