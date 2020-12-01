57ac14114ed1c7e15310051765f2f379a1e3ff98
Cyprien <roche.cyprien@gmail.com>
2020-12-01 17:47:47 +0000

    implement part1 and start part2

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -6,7 +6,48 @@ import java.util.Scanner
 
 class Day1(private val scanner: Scanner, private val printStream: PrintStream): Problem {
 
-    override fun solve(): Unit {
-        if (scanner.hasNext()) printStream.println(scanner.next())
+    private val input = mutableListOf<Int>()
+
+    private fun load(): Unit {
+        while (scanner.hasNext()) {
+            input.add(scanner.nextInt())
+        }
     }
+
+    override fun solve(): Unit {
+        solvePart2()
+    }
+
+    fun solvePart1() {
+        load()
+        val (p, q) = findPairThatSumsToTarget(input, target = 2020)
+        printStream.println(p * q)
+    }
+
+    fun solvePart2() {
+        load()
+        val (p, q, r) = findTripleThatSumsToTarget(input, target = 2020)
+        printStream.println(p * q * r)
+    }
+}
+
+fun findPairThatSumsToTarget(input: List<Int>, target: Int): Pair<Int, Int> {
+    val set = hashSetOf<Int>()
+    input.forEach {
+        val complement = target - it
+        if (set.contains(complement)) return Pair(complement, it)
+        set.add(it)
+    }
+    return Pair(-1,-1)
+}
+
+fun findTripleThatSumsToTarget(input: List<Int>, target: Int): Triple<Int, Int, Int> {
+    input.forEachIndexed { index, it ->
+        val (p, q) = findPairThatSumsToTarget(input.drop(index + 1), target - it)
+        if (p != -1)  {
+            return Triple(it, p, q)
+
+        }
+    }
+    return Triple(-1,-1, -1)
 }
