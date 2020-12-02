b0d3cb0ea2202493b18f1c8fa54082e7a53a570f
Cyprien <roche.cyprien@gmail.com>
2020-12-02 12:24:27 +0000

    add tests for part2 of day2

--- src/test/kotlin/day2/Day2Test.kt
+++ src/test/kotlin/day2/Day2Test.kt
@@ -11,16 +11,16 @@ class Day2Test {
     private val outContent = ByteArrayOutputStream()
 
     @Test
-    fun canRecogniseValidPassword() {
-        assertEquals(1, isValidPassword(1..4, 'a', "abc"))
-        assertEquals(1, isValidPassword(1..4, 'a', "aaaa"))
-        assertEquals(1, isValidPassword(1..2, 'z', "pazzword"))
+    fun canRecogniseValidPassword1() {
+        assertEquals(1, isValidPassword1(1,4, 'a', "abc"))
+        assertEquals(1, isValidPassword1(1,4, 'a', "aaaa"))
+        assertEquals(1, isValidPassword1(1,3, 'z', "pazzword"))
     }
 
     @Test
-    fun canRecogniseInvalidPassword() {
-        assertEquals(0, isValidPassword(1..4, 'a', "aaaaa"))
-        assertEquals(0, isValidPassword(1..2, 'z', "pazzzword"))
+    fun canRecogniseInvalidPassword1() {
+        assertEquals(0, isValidPassword1(1,4, 'a', "aaaaa"))
+        assertEquals(0, isValidPassword1(1,3, 'z', "pazzzzword"))
     }
 
     @Test
@@ -30,5 +30,29 @@ class Day2Test {
         Day2(scanner, printStream).solvePart1()
         val output = outContent.toString().trimMargin().toInt()
         assertEquals(2, output)
+    }
+
+    @Test
+    fun canRecogniseValidPassword2() {
+        assertEquals(1, isValidPassword2(1,4, 'a', "abcd"))
+        assertEquals(1, isValidPassword2(1,4, 'a', "baaa"))
+        assertEquals(1, isValidPassword2(1,3, 'z', "pazzword"))
+    }
+
+    @Test
+    fun canRecogniseInvalidPassword2() {
+        assertEquals(0, isValidPassword2(1,4, 'a', "abca"))
+        assertEquals(0, isValidPassword2(1,4, 'a', "bbbb"))
+        assertEquals(0, isValidPassword2(1,2, 'z', "pazzzzword"))
+    }
+
+
+    @Test
+    fun `can load example input and print correct output for part 2`() {
+        val scanner = getScanner(2, "test")
+        val printStream = PrintStream(outContent)
+        Day2(scanner, printStream).solvePart2()
+        val output = outContent.toString().trimMargin().toInt()
+        assertEquals(1, output)
     }
 }
