abfc4c7564edf5e1bce060652562c2304f5dc854
Cyprien <roche.cyprien@gmail.com>
2020-12-02 11:49:37 +0000

    implement part1 solution of day2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -4,9 +4,22 @@ import Problem
 import java.io.PrintStream
 import java.util.Scanner
 
-class Day2(private val scanner: Scanner, private val printStream: PrintStream): Problem {
+class Day2(private val scanner: Scanner, private val printStream: PrintStream) : Problem {
+
+    private val pattern = """(\d+)-(\d+) (\w): (\w+)"""
 
     override fun solvePart1() {
+        var count = 0
+        while (scanner.hasNext()) {
+            val input = scanner.nextLine()
+            println(input)
+
+            val match = Regex(pattern).find(input)!!
+            val (min, max, char, password) = match.destructured
+            val range = min.toInt().rangeTo(max.toInt())
+            count += isValidPassword(range, char[0], password)
+        }
+        printStream.println(count)
         return
     }
 
@@ -14,3 +27,6 @@ class Day2(private val scanner: Scanner, private val printStream: PrintStream): 
         return
     }
 }
+
+fun isValidPassword(range: IntRange, char: Char, password: String): Int =
+    if (password.count { it == char } in range) 1 else 0
