053c06e969ad2fd8b94c3293c3e29bde66f97e69
Cyprien <roche.cyprien@gmail.com>
2020-12-02 09:41:28 +0000

    add Day2 class and add in DayProblemFactory

--- src/main/kotlin/DayProblemFactory.kt
+++ src/main/kotlin/DayProblemFactory.kt
@@ -2,12 +2,14 @@ import java.io.PrintStream
 import java.lang.IllegalArgumentException
 import java.util.Scanner
 import day1.Day1
+import day2.Day2
 
 class DayProblemFactory {
 
     companion object {
         fun getDayProblem(dayNumber: Int, scanner: Scanner, printStream: PrintStream): Problem = when (dayNumber) {
             1 -> Day1(scanner, printStream)
+            2 -> Day2(scanner, printStream)
             else -> throw IllegalArgumentException()
         }
     }
---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,16 @@
+package day2
+
+import Problem
+import java.io.PrintStream
+import java.util.Scanner
+
+class Day2(private val scanner: Scanner, private val printStream: PrintStream): Problem {
+
+    override fun solvePart1() {
+        return
+    }
+
+    override fun solvePart2() {
+        return
+    }
+}
